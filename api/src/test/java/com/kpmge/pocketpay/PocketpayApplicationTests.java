4503f65145c52d1d640d8e303fa0f571db2b1b2d
KPMGE <jesuskevin254@gmail.com>
2024-05-25 22:00:06 -0300

    :sparkles: configure spring project

---
+++ api/src/test/java/com/kpmge/pocketpay/PocketpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpmge.pocketpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
